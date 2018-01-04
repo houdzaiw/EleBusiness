eb2fdcc0f328a357d648218649ab9461f4ef6897
李启辉 <evan@liqihuideiMac.local>
2018-01-04 16:31:13 +0800

    增加定时器和lazy

--- src/containers/product/screens/productsListScreen.js
+++ src/containers/product/screens/productsListScreen.js
@@ -6,19 +6,43 @@ import {
 } from 'react-native'
 
 class productsListScreen extends Component {
+
     constructor(props){
         super(props)
-
+        this.state = {
+            count:0,
+        }
     }
+
     static navigationOptions = {
         title: '产品',
         titleStyle: {color: '#ff00ff'},
         headerStyle:{backgroundColor:'red'}
     };
+    componentWillMount(){
+
+    }
+    componentDidMount() {
+        this.timer = setInterval(
+            () => {this.printCount(this.state.count += 1)},
+            500
+        );
+    }
+    printCount(count){
+        this.setState({
+            count:count,
+        })
+
+    }
+    componentWillUnmount() {
+        // 如果存在this.timer，则使用clearTimeout清空。
+        // 如果你使用多个timer，那么用多个变量，或者用个数组来保存引用，然后逐个clear
+        this.timer && clearInterval(this.timer);
+    }
     render(){
         return(
             <View style = {{backgroundColor:'#2c6',flex:1}}>
-                <Text>hello world</Text>
+                <Text>{this.state.count}</Text>
             </View>
         )
     }
--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -45,6 +45,7 @@ const MainScreenNavigator = TabNavigator({
 },{
     animationEnabled: false, // 切换页面时是否有动画效果
     tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
+    lazy: true, // 是否懒加载
     swipeEnabled: false, // 是否可以左右滑动切换tab
     backBehavior: 'none', // 按 back 键是否跳转到第一个Tab(首页)， none 为不跳转
     tabBarOptions: {
